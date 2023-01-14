5d22e71980ba92805bf5a8567c3dad2280e14e78
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-01-15 09:17:48 +1000

    Созданы страницы и вью функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Main page')
+
+
+def group_list(request):
+    return HttpResponse('Group list page')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Group posts page')
